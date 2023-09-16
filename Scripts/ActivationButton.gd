467318da5c3c6bb479a2d60a7e5591a82822bb71
bezbiletnym <61245298+bezbiletnym@users.noreply.github.com>
2023-09-16 16:33:18 +0400

    Working on button base class

--- Scripts/ActivationButton.gd
+++ Scripts/ActivationButton.gd
@@ -1,11 +1,16 @@
 extends TextureButton
+class_name InteractiveObject
 
 @export var anim_player: AnimationPlayer
 @export var isGoal: bool
+@export var actions: Array[String]
+@export var soundFile: Resource
 @onready var sceneManager = get_node("/root/Room/SceneTransition") #Верхняя нода всех комнат и вступлений должна называться Room
+@onready var audio = $AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if soundFile:
+		audio.stream = soundFile
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,11 +21,23 @@ func _process(delta):
 func _on_mouse_entered():
 	anim_player.play("hover")
 
-
 func _on_mouse_exited():
 	anim_player.play_backwards("hover")
 
 
-func _on_pressed():
+func _on_pressed():	
+	for action in actions:
+		match action:
+			"sound":
+				await play_sound()
+
 	if isGoal == true:
 		sceneManager.transition_to()
+	
+
+func play_sound():
+	if audio.stream: #check if the audiofile is loaded
+		audio.play()
+		await audio.finished
+	else:
+		print_debug("NO AUDIO")
