a3ba1275b6c7b2511e2fd5fdb0e45b0a78f01bfa
bezbiletnym <61245298+bezbiletnym@users.noreply.github.com>
2023-09-16 16:38:35 +0400

    Looped MainScene audiotheme

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 class_name Menu
 @onready var sceneManager = $SceneTransition
+@onready var audio = $AudioStreamPlayer2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,3 +15,7 @@ func _process(delta):
 
 func _on_play_button_pressed():
 	sceneManager.transition_to()
+
+
+func _on_audio_stream_player_2d_finished():
+	audio.play()
